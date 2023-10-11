5f527d382b079dee927bddb65a852ad1d580e653
szocslevente <szucs.levente80@gmail.com>
2023-10-11 16:19:12 +0200

    Initializing

--- src/test/java/com/cavity/usermanager/UserManagerApplicationTests.java
+++ src/test/java/com/cavity/usermanager/UserManagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsermanagerApplicationTests {
+class UserManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
